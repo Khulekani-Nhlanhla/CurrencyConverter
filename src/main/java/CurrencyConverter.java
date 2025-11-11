1624b9057193b1b59fe60323c0af4037546e1c13
khulekani-Nhlanhla <k.nhlanhla2030@gmail.com>
2025-11-11 18:27:52 +0200

    Added API configuration, UI component objects and array of supported currencies

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,2 +1,32 @@
-public class CurrencyConverter {
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CurrencyConverter extends JFrame implements ActionListener {
+
+    //API CONFIG
+    private static final String API_KEY = "dbe5ed09bcaefb9378619979";
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/";
+
+    //UI COMPONENTS
+    private JComboBox<String> fromCurrencyComboBox, toCurrencyCombo;
+    private JTextField amountField, resultField;
+    private JButton convertButton, swapButton;
+    private JLabel timeLabel;
+
+    //SUPPORTED CURRENCIES
+    private final String[] currencies = {
+            "USD", "EUR", "GBP", "JPY", "CAD",
+            "AUD", "CHF", "CNY", "INR", "SGD",
+            "MYR", "KRW", "THB", "IDR", "VND"
+    };
+
+
 }
